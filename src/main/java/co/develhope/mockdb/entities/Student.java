be521cba3308f5d481f7597bd684c7bcb135a026
lulanoise <lucrezia.rumore98@gmail.com>
2023-04-24 12:00:58 +0200

    removed lombok

--- src/main/java/co/develhope/mockdb/entities/Student.java
+++ src/main/java/co/develhope/mockdb/entities/Student.java
@@ -1,15 +1,9 @@
 package co.develhope.mockdb.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 @Entity
 @Table(name = "Student")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 
 public class Student {
 
@@ -24,4 +18,52 @@ public class Student {
     @Column(name = "email", unique = true)
     private String email;
 
+    public Student(long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
